f33ff7e5940d76473f7cd66e81ccefffedf8019b
KSMaster115 <64973076+KSMaster115@users.noreply.github.com>
2024-11-11 04:00:15 +0000

    hi

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 from flask import Flask
+from flask import render_template, request
+
+app = Flask(__name__)
+
+# Decorator
+# The / meanas that as long as you call a function from flask, it will go here first
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    return(render_template('index.html'))
+
+if __name__ == "__main__":
+    app.run()
